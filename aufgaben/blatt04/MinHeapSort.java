f0d44eb66fd648b1285e2a155b774b294848a60c
KhoalinhNguyen <97631922+KhoalinhNguyen@users.noreply.github.com>
2022-05-06 10:48:17 +0200

    Update MinHeapSort.java

--- aufgaben/blatt04/MinHeapSort.java
+++ aufgaben/blatt04/MinHeapSort.java
@@ -6,6 +6,8 @@ import java.time.Duration;
 
 public class MinHeapSort {
     public static void main(String[] args) {
+        
+        // Eingabe lesen und checken
         Scanner input = new Scanner(System.in);
         int k = Integer.parseInt(args[0]);
         ArrayList<Integer> list = new ArrayList<Integer>();
@@ -20,17 +22,23 @@ public class MinHeapSort {
                 System.err.println("Invalid Input");
             }
         }
+        
+        // ArrayList in int[] umwandeln
         int kth = 0;
         int[] array = new int[list.size()];
         for (int i = 0; i < array.length; i++) {
             array[i] = list.get(i);
         }
-        int[] array1 = array;
-        int[] array2 = array;
+        
+        // array1 für Laufzeitsmessung und array2 zum Prüfen von Assertion
+        int[] array1 = array.clone();
+        int[] array2 = array.clone();
+        
         Instant start1 = Instant.now();
         kth = heapSelect(array1, k);
         Instant finish1 = Instant.now();
         long time1 = Duration.between(start1, finish1).toMillis();
+        
         System.out.println("Time: " + time1);
         assert assertion(array2,k) == kth : "Es gibt einen Fehler";
 
@@ -67,12 +75,13 @@ public class MinHeapSort {
     }
 
     public static int extractMin(int[] data, int n) {
-        // Move current root to end
+        // Der Wurzel zum Ende bringen
         int temp = data[0];
         data[0] = data[n - 1];
         data[n - 1] = temp;
 
-        // call min heapify on the reduced heap
+        
+        // minHeapify auf das reduzierte Heap
         minHeapify(data, n-1, 0);
 
 
@@ -80,13 +89,17 @@ public class MinHeapSort {
     }
 
     public static int heapSelect(int [] data, int k) {
+        // Zuerst ein Min-Heap bauen
         buildMinHeap(data,data.length);
-        int kth =0;
-        for(int i = 0; i<k;i++) {
+        int kth = 0;
+        // Für jede Schleife nehmen wir das kleinste Element
+        // -> für die k-te Schleife wird das k-kleinste Element ausgewählt
+        for(int i = 0; i < k; i++) {
             kth = extractMin(data, data.length-i);
         }
         return kth;
     }
+    
     public static int assertion(int[] data, int k){
         Arrays.sort(data);
         return data[k-1];

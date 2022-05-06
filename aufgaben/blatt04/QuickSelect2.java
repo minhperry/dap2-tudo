f115727c8ee1097fdf77335d98a87394ac990a01
minhperry <46137516+minhperry@users.noreply.github.com>
2022-05-06 11:19:34 +0200

    Update QuickSelect2.java

--- aufgaben/blatt04/QuickSelect2.java
+++ aufgaben/blatt04/QuickSelect2.java
@@ -7,6 +7,8 @@ import java.util.Random;
 
 public class QuickSelect {
     public static void main(String[] args) {
+        
+        // Eingabe lesen und checken
         Scanner input = new Scanner(System.in);
         int k = Integer.parseInt(args[0]);
         String wort = args[1];
@@ -22,63 +24,72 @@ public class QuickSelect {
                 System.err.println("Invalid Input");
             }
         }
+        
+        // ArrayList -> int[]
         int[] array = new int[list.size()];
         for (int i = 0; i < array.length; i++) {
             array[i] = list.get(i);
         }
-        int[] array1 = array;
-        int[] array2 = array;
-        int[] array3 = array;
-        int[] array4 = array;
+        int[] array1 = array.clone();
+        int[] array2 = array.clone();
+        int[] array3 = array.clone();
+        int[] array4 = array.clone();
+        
         if (wort == "heap" || wort == "quickr" || wort == "quickf") {
             if (wort == "quickr") {
+                
                 Instant start1 = Instant.now();
                 int kth1 = quickSelectRand(array1, 0, array1.length-1, k-1);
                 Instant finish1 = Instant.now();
                 long time1 = Duration.between(start1, finish1).toMillis();
+                
                 System.out.println("TimeQuickr: " + time1);
                 assert assertion(array2,k) == kth1 : "Es gibt einen Fehler";
             } else if(wort == "quickf"){
+                
                 Instant start2 = Instant.now();
                 int kth2 = quickSelectFirst(array3, 0, array1.length-1, k-1);
                 Instant finish2 = Instant.now();
                 long time2 = Duration.between(start2, finish2).toMillis();
+                
                 System.out.println("TimeQuickf: " + time2);
                 assert assertion(array4,k) == kth2 : "Es gibt einen Fehler";
             }
         }
     }
+    
+    // Hilfsmethode zum Wählen von einer zufälligen Nummer zwischen min und max
     public static int rand(int min, int max)
     {
+        // min und max checken: min soll kleiner als max, oder die Zahlenbereich soll nicht die int-Grenze überschreiten
         if (min > max || (max - min + 1 > Integer.MAX_VALUE)) {
             throw new IllegalArgumentException("Invalid range");
         }
         return new Random().nextInt(max - min + 1) + min;
     }
 
+    // Hilfsmethode zum Tauschen
     public static void swap(int[] data, int i, int j)
     {
         int temp = data[i];
         data[i] = data[j];
         data[j] = temp;
     }
-
-    // Partition using Lomuto partition scheme
+    
     public static int partition(int[] data, int l, int p, int r)
     {
-        // pick `pIndex` as a pivot from the array
+        // Pivot als das gewählte Index
         int pivot = data[p];
 
-        // Move pivot to end
+        // Pivot zum Ende bringen
         swap(data, p, r);
-
-        // elements less than the pivot will be pushed to the left of `pIndex`;
-        // elements more than the pivot will be pushed to the right of `pIndex`;
-        // equal elements can go either way
+       
+        // ?
         p = l;
 
-        // each time we find an element less than or equal to the pivot, `pIndex`
-        // is incremented, and that element would be placed before the pivot.
+        // Das Array von links nach rechts iterieren. 
+        // kleinere oder gleiche Elemente soll links des Pivots, und größere Elemente rechts des Pivots verschoben
+        // wie Blatt2 Partitionierung
         for (int i = l; i < r; i++)
         {
             if (data[i] <= pivot)
@@ -88,36 +99,34 @@ public class QuickSelect {
             }
         }
 
-        // move pivot to its final place
+        // Pivot zur richtigen Position bringen und gibt die Position zurück
         swap(data, p, r);
-
-        // return `pIndex` (index of the pivot element)
         return p;
     }
 
     public static int quickSelectFirst(int[] data, int l, int r, int k)
     {
-        // If the array contains only one element, return that element
+        // Falls das Array nur ein Element hat, gibt dieses Element dann zurück
         if (l == r) {
             return data[l];
         }
 
-        // select a `pIndex` between left and right
+        // Pivot p = l wählen
+        // Nach die Partionierung steht das Pivot an der richtigen Position
         int p = data[l];
-
         p = partition(data, l, p, r);
 
-        // The pivot is in its final sorted position
+        // Falls diese die gesuchte Position ist dann geben wir die p- bzw. k-kleinste Element zurück
         if (k == p) {
             return data[k];
         }
 
-        // if `k` is less than the pivot index
+        // Falls k rechts des Pivots steht dann wählen wir aus der linken Seite des Pivots ...
         else if (k < p) {
             return quickSelectFirst(data, l, p - 1, k);
         }
 
-        // if `k` is more than the pivot index
+        // ... sonst rechten Seite
         else {
             return quickSelectFirst(data, p + 1, r, k);
         }
@@ -125,27 +134,21 @@ public class QuickSelect {
 
     public static int quickSelectRand(int[] data, int l, int r, int k)
     {
-        // If the array contains only one element, return that element
+        // Wie oben
         if (l == r) {
             return data[l];
         }
 
-        // select a `pIndex` between left and right
+        // Statt ein bestimmtes Pivot wählen wir ein zufälliges
         int p = rand(l, r);
-
         p = partition(data, l, p, r);
 
-        // The pivot is in its final sorted position
         if (k == p) {
             return data[k];
         }
-
-        // if `k` is less than the pivot index
         else if (k < p) {
             return quickSelectRand(data, l, p - 1, k);
         }
-
-        // if `k` is more than the pivot index
         else {
             return quickSelectRand(data, p + 1, r, k);
         }

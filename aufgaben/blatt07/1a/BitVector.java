bc7e28fb3dc05bfd4a7131d23ee8c3389fb6f9f9
KhoalinhNguyen <97631922+KhoalinhNguyen@users.noreply.github.com>
2022-05-27 10:17:55 +0200

    Update BitVector.java

--- aufgaben/blatt07/1a/BitVector.java
+++ aufgaben/blatt07/1a/BitVector.java
@@ -4,28 +4,32 @@ import java.math.*;
 class BitVector {
     private int size;
     private int[] bits;
-
+    
     public BitVector(int n) {
         size = n;
         bits = new int[(int) Math.ceil(n / 32.0)];
     }
-
+   
     public int size() {
         return size;
     }
 
     public boolean get(int index) {
         int which = bits[index / 32];
+        
+        //das Bit nach ganz hinten rechts verschieben
         int shifted = which >> index;
         return (shifted & 1) > 0;
     }
 
     public void set(int index, boolean value) {
         int which = bits[index / 32];
+        //1 zu dem richtigen Bit verchieben
         int mask = 1 << index;
         if (value) {
             which = which | mask;
         } else {
+            //mask -> 0
             mask = ~mask;
             which = which & mask;
         }
